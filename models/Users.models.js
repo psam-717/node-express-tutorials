cf3b7b09ceeade35d2f20ba45efad6ce77bd14fd
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-05-08 21:49:48 +0000

    set up database connection and funtionality for sign up

---
+++ models/Users.models.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    // otherName: {
+    //     type: String,
+    //     // required: true
+    // },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    jobDescription: {
+        type: String,
+        required: true,
+        enum : ['frontend', 'backend', 'none'],
+        default: 'none'
+    }
+    
+},{
+    timestamps: true,
+})
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,3 +1,78 @@
+import User from "../../models/users.models.js";
+import bcrypt from "bcrypt";
+
+export const signUp = async( req, res) => {
+    const {firstName, lastName, email, password, jobDescription} = req.body;
+    
+    try {
+        
+        if(!firstName || !lastName || !email || !password || !jobDescription){
+            return res.status(400).json({
+                success: false,
+                message: 'Please provide data for all fields'
+            })
+        }
+
+    
+        const existingUser = await User.findOne({email});
+
+        if(existingUser){
+            return res.status(401).json({
+                success: false, 
+                message: 'User already exists'
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+            jobDescription
+
+        })
+        await newUser.save();
+
+        const newUserWithoutPassword = {...newUser.toObject(), password: undefined};
+
+        return res.status(201).json({
+            success: true,
+            message: 'User has been created successfully',
+            data: newUserWithoutPassword
+        });
+       
+        
+    } catch (error) {
+        console.log('Error caused by: ', error,);
+        return res.status(500).json({success: false, message: 'Internal server error occurred while signing up'});
+    }
+}
+
+export const getAllUsers = async(req, res) => {
+    try {
+        const allUsers = await User.find().select('-password');
+
+        if(!allUsers || allUsers.length === 0 ){
+            return res.status(400).json({
+                success: false, 
+                message: 'No users found'
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: 'Users data retrieved successfully',
+            data: allUsers
+        })
+
+    } catch (error) {
+        console.log('Error caused by: ', error,);
+        return res.status(500).json({success: false, message: 'Internal server error occurred while getting all users'});
+    }
+}
+
 export const displayHomeMessage = async (req, res) => {
     try {
         return res.status(200).json({
