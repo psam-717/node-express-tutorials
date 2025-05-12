37a45546b63a299e021ba51890e08acb5d14f0ea
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-05-12 21:25:39 +0000

    included the morgan package

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import usersRoutes from './routes/users.route.js'
 import { connectToDB } from "../config/mongodb.js";
+import morgan from "morgan";
 
 const port = 3000;
 
 const app = express();
 app.use(express.json());
+
+app.use(morgan('dev'))
 
 connectToDB();
 app.use('/api', usersRoutes);
