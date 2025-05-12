574fa1067fcbb328bbed7c20c80f585237b08a71
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-05-12 21:23:12 +0000

    used req.query and req.params to retrieve data from the db

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,19 +1,13 @@
+import mongoose from "mongoose";
 import User from "../../models/users.models.js";
 import bcrypt from "bcrypt";
+
 
 export const signUp = async( req, res) => {
     const {firstName, lastName, email, password, jobDescription} = req.body;
     
     try {
         
-        if(!firstName || !lastName || !email || !password || !jobDescription){
-            return res.status(400).json({
-                success: false,
-                message: 'Please provide data for all fields'
-            })
-        }
-
-    
         const existingUser = await User.findOne({email});
 
         if(existingUser){
@@ -72,6 +66,86 @@ export const getAllUsers = async(req, res) => {
         return res.status(500).json({success: false, message: 'Internal server error occurred while getting all users'});
     }
 }
+
+
+export const getSingleUser = async(req, res) => {
+    try {
+        const {id} = req.params;
+
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400).json({
+                success: false,
+                message: 'Invalid id specified'
+            });
+        }
+
+        const user = await User.findById(id);
+
+        //if user is not in our database
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: 'User not found'
+            })
+        }
+
+        //when user is present
+        return res.status(200).json({
+            success: true,
+            message: 'User data retrieved',
+            data: user
+        })
+        
+    } catch (error) {
+        console.log("Error caused by: ", error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error while displaying user data"
+        })
+    }
+
+}
+
+
+//req.query
+export const getUserByJobDescription = async (req, res) => {
+    try {
+        const {jobDescription} = req.query;
+
+        if(!jobDescription){
+            return res.status(400).json({
+                success: false,
+                message: "Job description has not been specified"
+            })
+        }
+
+        //getting user
+        const user = await User.find({jobDescription});
+
+        if(!user || user.length === 0){
+            return res.status(404).json({
+                success: false,
+                message: 'No users found',
+                
+            })
+        }
+
+        //if such users exist
+        return res.status(200).json({
+            success: true,
+            message: 'Users retrieved',
+            data: user
+        })
+
+    } catch (error) {
+        console.log("Error caused by: ", error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error while getting user with job description"
+        })
+    }
+}
+
 
 export const displayHomeMessage = async (req, res) => {
     try {
