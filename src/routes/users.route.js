267c67c6613422cc26586dfbfb7b87b33cd2d51e
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-05-19 17:06:50 +0000

    routes to login included

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createDummyUser, displayHomeMessage, getAllUsers, getSingleUser, getUserByJobDescription, signUp } from "../controllers/users.controllers.js";
+import { createDummyUser, displayHomeMessage, getAllUsers, getSingleUser, getUserByJobDescription, login, signUp } from "../controllers/users.controllers.js";
 import { signUpValidation } from "../../validators/signup.validation.js";
 import { validate } from "../../validators/validate.js";
 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get('/home',displayHomeMessage);
 router.get('/get-user-data', createDummyUser);
 router.post('/signup',signUpValidation, validate ,signUp);
+router.post('/login', login)
 router.get('/get-all-users', getAllUsers)
 router.get('/single-user/:id', getSingleUser)
 router.get('/user-jd', getUserByJobDescription)
